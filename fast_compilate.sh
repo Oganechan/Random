a6bca356eb6d996c33e45eaf13f74f381847d9aa
Oganechan <wantedman1205@gmail.com>
2026-07-07 12:05:35 +1000

    script: add debug mode

--- fast_compilate.sh
+++ fast_compilate.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+debug=1
+
+if [[ "$1" == "-d" || "$1" == "--debug" ]]; then
+    echo "Optimization off"
+    debug=0
+else
+    echo "Optimization on"
+fi
+
 cd $(dirname $0)
 mkdir -p build
 
 for cpp in $(find ./examples -name "*.cpp"); do
     filename=$(basename "$cpp" .cpp)
-    g++ "$cpp" -o "./build/$filename"
+    if [[ $debug -eq 0 ]]; then
+        g++ "$cpp" -o "./build/$filename"
+    else
+        g++ -O3 "$cpp" -o "./build/$filename"
+    fi
 done 
